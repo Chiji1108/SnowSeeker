35fec9a4bef1f272dd0ab44eda3a08bae5afde25
chiji1108 <shingo.pad@gmail.com>
2025-03-14 17:59:13 +0900

    ContentView.swiftを更新し、ユーザー情報を表示するUserViewを追加しました。画面サイズに応じて表示方法を変更するレイアウトを実装しました。

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -7,26 +7,30 @@
 
 import SwiftUI
 
-struct User: Identifiable {
-    var id = "Taylor Swift"
+struct UserView: View {
+    var body: some View {
+        Group {
+            Text("Name: Paul")
+            Text("Country: England")
+            Text("Pets: Luna and Arya")
+        }
+        .font(.title)
+    }
 }
 
 struct ContentView: View {
-    @State private var selectedUser: User? = nil
-    @State private var isShowingUser = false
-    
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+
     var body: some View {
-        Button("Tap Me") {
-            selectedUser = User()
-            isShowingUser = true
+        if horizontalSizeClass == .compact {
+            VStack {
+                UserView()
+            }
+        } else {
+            HStack {
+                UserView()
+            }
         }
-        .sheet(item: $selectedUser) { user in
-            Text(user.id)
-                .presentationDetents([.medium, .large])
-        }
-//        .alert("Welcome", isPresented: $isShowingUser, presenting: selectedUser) { user in
-//            Button(user.id) { }
-//        }
     }
 }
 

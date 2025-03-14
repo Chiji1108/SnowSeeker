32c6968eb824dcb2b752f4554589b3337cd2a8ed
chiji1108 <shingo.pad@gmail.com>
2025-03-14 18:04:55 +0900

    ContentView.swiftを更新し、ユーザー名の検索機能を追加しました。リスト表示と検索バーを実装し、ユーザーが名前をフィルタリングできるようにしました。

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -7,29 +7,25 @@
 
 import SwiftUI
 
-struct UserView: View {
-    var body: some View {
-        Group {
-            Text("Name: Paul")
-            Text("Country: England")
-            Text("Pets: Luna and Arya")
-        }
-        .font(.title)
-    }
-}
-
 struct ContentView: View {
-    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    @State private var searchText = ""
+    let allNames = ["Subh", "Vina", "Melvin", "Stefanie"]
+
+    var filteredNames: [String] {
+        if searchText.isEmpty {
+            allNames
+        } else {
+            allNames.filter { $0.localizedStandardContains(searchText) }
+        }
+    }
 
     var body: some View {
-        if horizontalSizeClass == .compact {
-            VStack {
-                UserView()
+        NavigationStack {
+            List(filteredNames, id: \.self) { name in
+                Text(name)
             }
-        } else {
-            HStack {
-                UserView()
-            }
+            .searchable(text: $searchText, prompt: "Look for something")
+            .navigationTitle("Searching")
         }
     }
 }
